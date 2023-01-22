b2ff26536b10b44b2cf880b924e5c0490f9ac5e5
FifthEpoch <macrohard.tv@gmail.com>
2023-01-21 20:32:52 -0800

    NER tested

--- WK03/CH08/NER-movie-titles.py
+++ WK03/CH08/NER-movie-titles.py
@@ -5,9 +5,11 @@ import nltk
 nltk.download('brown')
 nltk.download('movie_reviews')
 from nltk.corpus import brown, movie_reviews
+from nltk.tokenize import word_tokenize
+
 def get_top_250_list():
     titles = []
-    with open("./movie-titles/IMDB-top-250.txt") as file:
+    with open("./data/IMDB-top-250.txt") as file:
         for line in file:
             words = line.split()
             title = ""
@@ -20,7 +22,7 @@ def get_top_250_list():
     return titles
 
 def get_top_397_list():
-    with open("movie-titles/IMDB-top-397.csv", 'r') as file:
+    with open("./data/IMDB-top-397.csv", 'r') as file:
         csvreader = csv.reader(file)
         collected_titles = []
         for row in csvreader:
@@ -33,7 +35,7 @@ def get_top_397_list():
     return collected_titles[1:]
 
 def get_top_1000_list():
-    with open("movie-titles/IMDB-top-1000.csv", 'r') as file:
+    with open("./data/IMDB-top-1000.csv", 'r') as file:
         csvreader = csv.reader(file)
         collected_titles = []
         for row in csvreader:
@@ -43,16 +45,25 @@ def get_top_1000_list():
                 collected_titles.append(raw_title.split())
     return collected_titles[1:]
 
-def label_BIO(_samples, _NE):
+def get_data_from_file(_fn):
+    with open(_fn, 'r') as file:
+        data = file.read().replace('\n', ' ')
+    return data
+
+def label_BIO(_tokens, _NE):
     # MOV: movie title
     # beginning of a movie title is tagged with "B-MOV"
     # middle/end of a movie title is tagged "I-MOV"
 
     BIO_for_samples = []
-    sample_size = len(_samples)
+    sample_size = len(_tokens)
+
+    current_i = 0
 
     # search through each sample
     for i in range(sample_size):
+
+        if i != current_i: continue
 
         # search each named entity for each sample
         has_match = False
@@ -60,7 +71,7 @@ def label_BIO(_samples, _NE):
         for j in range(len(_NE)):
 
             # if the first word is a match
-            if _NE[j][0] == _samples[i][0]:
+            if _NE[j][0] == _tokens[i]:
 
                 # if the matched movie title does not
                 # exceed the length of the sample
@@ -69,7 +80,7 @@ def label_BIO(_samples, _NE):
                     # check if all other components match
                     no_match = False
                     for k in range(1, len(_NE[j])):
-                        if _NE[j][k] != _samples[i + k][0]:
+                        if _NE[j][k] != _tokens[i + k]:
                             no_match = True
                             break
 
@@ -81,33 +92,45 @@ def label_BIO(_samples, _NE):
 
         # Done searching through the Named Entity List
         if has_match:
-            BIO_for_samples.append((_samples[i][0], 'B-MOV'))
+            BIO_for_samples.append((_tokens[i], 'B-MOV'))
             for j in range(1, len(_NE[match_index])):
-                BIO_for_samples.append((_samples[i][0], 'I-MOV'))
+                BIO_for_samples.append((_tokens[i + j], 'I-MOV'))
+            current_i += len(_NE[match_index])
         else:
-            BIO_for_samples.append((_samples[i][0], 'O'))
+            BIO_for_samples.append((_tokens[i], 'O'))
+            current_i += 1
 
     return BIO_for_samples
 
+def print_BIO_res(_BIO):
+    for i in range(len(_BIO)):
+        if _BIO[i][1] == 'B-MOV':
+            for j in range(i - 7, i + 7):
+                if _BIO[j][1] == 'O':
+                    print(_BIO[j][0], end=" ")
+                else:
+                    print(_BIO[j], end=" ")
+            print("")
 
 if __name__ == "__main__":
+
+    # all lists from IMDB
     titles_top_250 = get_top_250_list()
     titles_top_397 = get_top_397_list()
     titles_top_1000 = get_top_1000_list()
 
-    review_file_ids = movie_reviews.fileids()
-    REVIEWS = movie_reviews.raw(review_file_ids[2])
-    print(f'REVIEWS: {REVIEWS}')
+    # uncomment for movie reviews resources
+    # review_file_ids = movie_reviews.fileids()
+    # REVIEWS = movie_reviews.raw(review_file_ids[2])
 
-    CORPUS = brown.tagged_words(categories='reviews', tagset='universal')
-    CORPUS_SIZE = len(brown.tagged_words(categories='reviews'))
-
-    CUT_OFF = math.floor(CORPUS_SIZE * 0.75)
-
-    # section off training and testing lists from corpus
-    training_list = CORPUS[:CUT_OFF]
-    testing_list = CORPUS[CUT_OFF:]
-
+    # get text data from a text file
+    data = get_data_from_file("data/article-about-a-genre.txt")
+    # tokenize text data
+    tokens = word_tokenize(data)
     # tag with BIO using the IMDB top 1000 movie title list
-    BIO = label_BIO(testing_list, titles_top_1000)
-    print(BIO)
+    BIO = label_BIO(tokens, titles_top_1000)
+
+    # B-MOV marks the beginning of a known movie title
+    # I-MOV marks the middle/end of a known movie title
+    # O for all other words
+    print_BIO_res(BIO)
